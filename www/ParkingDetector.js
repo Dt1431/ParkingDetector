bd32c5f4682c617199d1503ffcff2f86133ab3a3
David Turner <daveturner81@gmail.com>
2016-07-07 17:35:53 -0500

    js fix 3

--- www/ParkingDetector.js
+++ www/ParkingDetector.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-window.initParkingDetectorPlugin = function(showMessages, askedForConformationMax, maxPrompts, endpoint, success, error) {
+window.initParkingDetectorPlugin = function(showMessages, maxPrompts, endpoint, success, error) {
     exec(success, error, "ParkingDetector", "initPlugin", [showMessages, maxPrompts, endpoint]);
 };
